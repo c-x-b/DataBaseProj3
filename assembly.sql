e6a92696296317af2f0c93be6aaa930757f2c988
ling23187 <80144329+ling23187@users.noreply.github.com>
2023-05-23 00:30:48 +0800

    I have no idea about the last process. It is quite ambiguous.

--- assembly.sql
+++ assembly.sql
@@ -1,14 +1,71 @@
-create table Assembly(
-id  int not null primary key auto_increment, 
-part char(20),
-subpart char(20));
-insert into Assembly values 
-(1,'trike','wheel'),
-(2,'trike','frame'),
-(3,'frame','seat'),
-(4,'frame','pedal'),
-(5,'wheel','spoke'),
-(6,'wheel','tire'),
-(7,'tire','rim'),
-(8,'tire','tube');
-select * from assembly;
+-- Active: 1682927233860@@162.105.146.37@43306@stu2000012965
+CREATE TABLE Assembly(
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    part char(20),
+    subpart char(20)
+);
+
+INSERT INTO
+    Assembly
+VALUES
+    (1, 'trike', 'wheel'),
+    (2, 'trike', 'frame'),
+    (3, 'frame', 'seat'),
+    (4, 'frame', 'pedal'),
+    (5, 'wheel', 'spoke'),
+    (6, 'wheel', 'tire'),
+    (7, 'tire', 'rim'),
+    (8, 'tire', 'tube');
+
+SELECT
+    *
+FROM
+    Assembly;
+
+WITH recursive Components (part, subpart) AS (
+    SELECT
+        part,
+        subpart
+    FROM
+        Assembly
+    UNION
+    ALL
+    SELECT
+        A.part,
+        C.subpart
+    FROM
+        Assembly A,
+        Components C
+    WHERE
+        A.subpart = C.part
+)
+SELECT
+    *
+FROM
+    Components
+WHERE
+    part = ' trike ';
+
+WITH RECURSIVE cte (part, subpart, LEVEL) AS (
+    SELECT
+        part,
+        subpart,
+        1
+    FROM
+        Assembly
+    WHERE
+        part = 'trike'
+    UNION
+    ALL
+    SELECT
+        a.part,
+        a.subpart,
+        cte.level + 1
+    FROM
+        Assembly a
+        INNER JOIN cte ON a.part = cte.subpart
+)
+SELECT
+    *
+FROM
+    cte;
